c1efc5456339d673511d7f15ce9e7071f6629eff
Amer <amer1jawabra@gmail.com>
2024-02-25 17:21:33 -0500

    updated code

--- Assigment1-master/app/core/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-require('app/core/App.php');
-require('app/core/Controller.php');
-require('app/core/autoload.php');
--- Assigment1-master/app/views/Count/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simulate counting page loads
-$count = 100; // For example, you can use any mechanism to count page loads
-
-// Return the count as JSON-encoded data
-header('Content-Type: application/json');
-echo json_encode(['count' => $count]);
-?>
